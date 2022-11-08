651597267907b680c7aaf5d2a18db699fb5311fe
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-08 16:55:17 +0900

    mid-creating task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :create]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks/:id', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
